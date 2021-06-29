91f3a628b3c0cf61476983bfeee5db302dfa19e9
Ajshubham <76531082+Ajshubham@users.noreply.github.com>
2021-06-29 08:46:25 +0530

    Update 12.c
    change to fractional value

--- 12.c
+++ 12.c
@@ -5,6 +5,6 @@ int main(){
     float volume, r;
     printf("Enter radius in metre: ");
     scanf("%f",&r);
-    volume = (0.75 * 3.14) * (r * r * r);
+    volume = (3.0/4 * 3.14) * (r * r * r);
     printf("Volume of sphere of radius %f metre = %.2f metre cube", r,volume);
 }
